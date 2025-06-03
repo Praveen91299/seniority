a0b0311df8a612ef7992ae90e1b8ece8bd9ec0c4
Smik Patel <smikpatel@gmail.com>
2025-06-03 00:45:21 -0400

    implemented covariance matrix

--- utils_partitioning.py
+++ utils_partitioning.py
@@ -17,6 +17,7 @@ from utils_si_ics import (
     OverlappingAuxiliary,
     OverlappingGroups
 )
+import scipy.sparse._csr
 
 def is_qwc_hamiltonian(H):
     """
@@ -119,4 +120,37 @@ def augment_decomp_with_pauli_x_plus_i_pauli_y(decomp, N):
     x  = Q(f'X{N}')
     iy = 1j * Q(f'Y{N}')
     return [Op * x for Op in decomp] + [Op * iy for Op in decomp]
+
+# SI-ICS code
+# potential flaw: Hamiltonians now have complex coefficients, so this code may not work
+
+def convert_QubitOperator_to_BinaryHamiltonian(H):
+    Htequila = tq.QubitHamiltonian.from_openfermion(H)
+    Hbinary  = tq.grouping.binary_rep.BinaryHamiltonian.init_from_qubit_hamiltonian(Htequila, ignore_const=True)
+
+    return Hbinary
+
+def calculate_cov_dict(state, Hbin, Nqubits):
+    """
+    state is a 2**N vector or a csr_matrix
+    Hbin is a BinaryHamiltonian (Tequila)
+
+    return: dictionary of (bin1, bin2) : Cov_{Op[bin1], Op[bin2]}
+
+    note: if state is a csr_matrix, this function will convert it to a vector
+    """
+    if isinstance(state, scipy.sparse._csr.csr_matrix):
+        state = state.toarray()[0]
+
+    cov_dict = dict()
+
+    for i, term1 in enumerate(Hbin.binary_terms):
+        Op1 = get_sparse_operator(Q(term1.to_pauli_strings().key_openfermion()), Nqubits)
+        for j, term2 in enumerate(Hbin.binary_terms):
+            if i >= j and term1.commute(term2):
+                Op2 = get_sparse_operator(Q(term2.to_pauli_strings().key_openfermion()), Nqubits)
+                cov = expectation(Op1 @ Op2, state) - expectation(Op1, state) * expectation(Op2, state)
+                cov_dict[(term1.binary_tuple(), term2.binary_tuple())] = cov
+
+    return cov_dict
 
